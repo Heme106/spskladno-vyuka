fddcb5a37519372ecc78a0ef4715e38878d061e4
Heme106 <jakubhemerka12@gmail.com>
2026-09-14 09:44:25 +0200

    update:main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+# Ukázka funkce input()
+print("Jak se jmenujes?")
+input()
+jmeno = input("Jak se jmenuješ? ")
+jidlo = input("Jaké je tvé oblíbené jídlo? ")
+
+print()
+print("Ahoj,", jmeno + "!")
+print("Tvé oblíbené jídlo je:", jidlo)
