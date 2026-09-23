3b98dc335c5441bf7591f6f613e6da5cb4eac1b6
Heme106 <jakubhemerka12@gmail.com>
2026-09-23 10:52:10 +0200

    update:main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Ukázka funkce input()
+"""
 print("Jak se jmenujes?")
 input()
 jmeno = input("Jak se jmenuješ? ")
@@ -6,4 +7,13 @@ jidlo = input("Jaké je tvé oblíbené jídlo? ")
 
 print()
 print("Ahoj,", jmeno + "!")
-print("Tvé oblíbené jídlo je:", jidlo)
+print("Tvé oblíbené jídlo je:", jidlo
+"""
+
+prvni = input("Zadej první číslo")
+druhy = input("Zadej druhé číslo")
+prvni = float(prvni)
+druhy = float(druhy)
+print("součet:", prvni+druhy)
+print("rozdíl:", prvni-druhy)
+print("součin:", prvni*druhy)
